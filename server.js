ef328919ddf2079c1332c32a6d1f04cdeb4839e8
LeMinhThanh0801 <90897650+LeMinhThanh0801@users.noreply.github.com>
2021-11-03 00:46:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/classes', classesRoutes);
-
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
+const PORT=process.env.PORT||3000
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
